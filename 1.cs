a7acab6c203c684e8cf545bfa4bc3bf55a8320dc
timofey <timofey.gusev.opal@gmail.com>
2020-03-18 20:52:52 +0400

    первое задание исправлено

--- 1.cs
+++ 1.cs
@@ -3,12 +3,12 @@ namespace Sorting
 {
     class Programm
     {
-     
+
 
         public static void Main(string[] args)
         {
-           
-        int[] array = new int[];
+
+            int[] array = new int[10];
             array[0] = 25;
             array[1] = 15;
             array[2] = 154;
@@ -20,23 +20,35 @@ namespace Sorting
             array[8] = 14;
             array[9] = 10;
 
+            var result = Inciment(array);
+
+        }
+
+
+        public static int[] Inciment(int[] array)
+        {
+            int t;
 
             for (int i = 0; i < array.Length; i++)
             {
-                for (int j = i+1; j<array.Length; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    if(array[i]> array [j])
+                    if (array[i] > array[j])
                     {
-                        int t = array[i];
+                        t = array[i];
                         array[i] = array[j];
                         array[j] = t;
                     }
                 }
                 Console.WriteLine(array[i] + ", ");
             }
-            
-            Console.ReadKey();
-
+            return array;
         }
+
+
+
+
+
+
     }
 }
