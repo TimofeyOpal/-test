daa7cabc297d0b146e4c37b28d9960a4992a3218
timofey <timofey.gusev.opal@gmail.com>
2020-03-20 02:01:59 +0400

    второе задание готово

--- 2.cs
+++ 2.cs
@@ -1,13 +1,85 @@
 using System;
-using System.Linq;
-
-public class Test
+namespace Sorting
 {
-	public static void Main()
-	{
-		var array = new int[] { 8, 4, 2, 44, 3, 10 };
-		int name;
-		name = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine(array.Contains(name) ? "Есть" : "Отсутствует");
-	}
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+ 
+            int[] array = new int[10];
+            array[0] = 25;
+            array[1] = 15;
+            array[2] = 154;
+            array[3] = 145;
+            array[4] = 19;
+            array[5] = 88;
+            array[6] = 5;
+            array[7] = 45;
+            array[8] = 14;
+            array[9] = 10;
+			
+         var result = Inciment(array);
+			
+			Programm.Binary(array, 0, 0);
+			
+        }
+        public static int[] Inciment(int[] array)
+        {
+            int t;
+ 
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+                    }
+                }
+                Console.WriteLine(array[i] + ", ");
+            }
+            return array;
+        }
+        public static void Binary(int[] array, int left, int right)
+        {
+            left = 0; 
+            right = array.Length - 1; 
+            int index = 0; 
+ 
+            int q = Convert.ToInt32(Console.ReadLine());
+            int res = -1;
+ 
+            while (left <= right)
+            {
+                index = (right + left) / 2; 
+ 
+                
+                if (array[index] == q)
+                {
+                    res = index;
+                    break;
+                }
+                
+                if (array[index] < q)
+                {
+                    left = index + 1;
+                }
+                else
+                {
+                    right = index - 1;
+                }
+            }
+ 
+            if (res != -1)
+            {
+                Console.WriteLine("Присутсвует индекс: " + res);
+            }
+            else
+            {
+                Console.WriteLine("Отсутсвует");
+            }
+        }
+    }
 }
